32dca84203c89f5a6d0759ef651c3a7c8cbafb02
Jeremiah K. Kurwa <92439632+Jeremiah-Katumo@users.noreply.github.com>
2025-04-18 16:34:12 +0300

    created main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+
+from pet import Pet
+
+my_pet = Pet("Zuchu")
+
+my_pet.get_status()
+my_pet.eat()
+my_pet.play()
+my_pet.sleep()
+my_pet.train("roll over")
+my_pet.train("sit")
+my_pet.show_tricks()
+my_pet.get_status()
+
